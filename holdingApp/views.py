676efac7b978a0c04561a6e78b5d7c6f7c205a1b
MenacingDwarf <MenacingDwarf@yandex.ru>
2019-03-31 00:29:01 +0300

    Время до начала квеста Получение времени до начала квеста для ведения обратного отсчёта

--- holdingApp/views.py
+++ holdingApp/views.py
@@ -1,8 +1,21 @@
 from django.shortcuts import render, redirect
-from datetime import datetime
+from django.utils import timezone
 from preparationApp.models import Quest
 
 
+def parse_date(timedelta):
+    if timedelta.seconds < 0:
+        return 'started'
+    else:
+        days = int(timedelta.days)
+        hours = (timedelta.seconds - days * 24) // 3600
+        minutes = (timedelta.seconds - days * 24 - hours * 3600) // 60
+        seconds = timedelta.seconds - days * 24 - hours * 3600 - minutes * 60
+        return {'days': days, 'hours': hours, 'minutes': minutes, 'seconds': seconds}
+
+
 def current_puzzle(request, quest_id):
+    print(Quest.objects.get(id=quest_id).start_date - timezone.now())
     return render(request, 'holdingApp/currentPuzzlePage.html',
-                  {'current_time': datetime.utcnow(), 'quest': Quest.objects.get(id=quest_id)})
+                  {'to_quest': parse_date(Quest.objects.get(id=quest_id).start_date - timezone.now()),
+                   'quest': Quest.objects.get(id=quest_id)})

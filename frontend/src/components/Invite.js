ece80bd6c2b39af2278f265b943d9dc4af16d1c8
MenacingDwarf <MenacingDwarf@yandex.ru>
2019-06-12 21:42:28 +0300

    Team and inviting style and accepting invite button

--- frontend/src/components/Invite.js
+++ frontend/src/components/Invite.js
@@ -5,9 +5,15 @@ class Invite extends Component {
         let invite = this.props.invite;
         return (
             <div>
-                <h3>{invite.team.title}</h3>
-                Капитан: {invite.team.captain}
-                <div className="line"/>
+                <div className={"invite-place"}>
+                    <h3>{invite.team.title}</h3>
+                    Капитан: {invite.team.captain}
+                    <div className="line"/>
+                </div>
+                <div className={"invite-button"} onClick={() => window.location.href = "/teams/accept/invite/"+invite.id}>
+                    <div className="button-content">Принять</div>
+                    <div className="underline"/>
+                </div>
             </div>
         )
     }
--- frontend/src/components/Team.js
+++ frontend/src/components/Team.js
@@ -3,11 +3,17 @@ import React, {Component} from 'react'
 class Team extends Component {
     render() {
         let team = this.props.team;
+        let members = this.props.team.members.map((member, index) => (
+            <li key={index}>{member}</li>
+        ));
         return (
-            <div>
+            <div className={"team-place"}>
                 <h3>{team.title}</h3>
-                Капитан: {team.captain}
-                <div className="line"/>
+                <p><b>Капитан</b>: {team.captain}</p>
+                <p><b>Состав команды</b>:</p>
+                <ul style={{listStyleType: "none"}}>
+                    {members}
+                </ul>
             </div>
         )
     }
